20c42841df25f0cb69d2e801fc4d68c05cd87926
edu1010 <39130123+edu1010@users.noreply.github.com>
2022-02-26 15:18:24 +0100

    bases fish

--- Assets/FSMs/Fish/FSM_FISH.cs
+++ Assets/FSMs/Fish/FSM_FISH.cs
@@ -10,18 +10,22 @@ namespace FSM
     {
         public enum State
         {
-            INITIAL
+            INITIAL,EAT,HIDE
         };
 
         public State currentState = State.INITIAL;
         private FISH_Blackboard blackboard;
         private Arrive arrive;
         private Flocking flocking;
+
+        private FSM_EAT_EAT_PLANKTON eatBehievour;
+        private FSM_HIDE hideBehievour;
         
         // Start is called before the first frame update
         void Start()
         {
-            
+            eatBehievour = GetComponent<FSM_EAT_EAT_PLANKTON>();
+            hideBehievour = GetComponent<FSM_HIDE>();
         }
         public override void Exit()
         {
@@ -40,7 +44,11 @@ namespace FSM
             switch (currentState)
             {
                 case State.INITIAL:
-                    ChangeState(State.INITIAL);
+                    ChangeState(State.EAT);
+                    break;
+                case State.EAT:
+                    break;
+                case State.HIDE:
                     break;
             }
         }
@@ -51,11 +59,27 @@ namespace FSM
             // EXIT STATE LOGIC. Depends on current state
             switch (currentState) 
             {
+                case State.INITIAL:
+                    ChangeState(State.EAT);
+                    break;
+                case State.EAT:
+                    eatBehievour.Exit();
+                    break;
+                case State.HIDE:
+                    hideBehievour.Exit();
+                    break;
+
             }
 
             // ENTER STATE LOGIC. Depends on newState
             switch (newState)
-            { 
+            {
+                case State.EAT:
+                    eatBehievour.ReEnter();
+                    break;
+                case State.HIDE:
+                    hideBehievour.ReEnter();
+                    break;
             }
             currentState = newState;
         }
