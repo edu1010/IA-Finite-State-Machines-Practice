90ec2bd5bf6e99d1e75a656cf2cdff72f916075a
marinamcb <mcami@edu.tecnocampus.cat>
2022-02-27 17:39:20 +0100

    Turtle fsm created and turtle blackboard

---
+++ Assets/FSMs/Tortoise/TURTLE_BLACKBOARD.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TURTLE_Blackboard : MonoBehaviour
+{
+    [Header("Wander")]
+    public float timeout = 5.0f;
+
+    [Header("Breathe")]
+    public GameObject surface;
+    public float maxOxigen = 100.0f;
+    public float surfaceReachedRadius = 2.0f;
+}
