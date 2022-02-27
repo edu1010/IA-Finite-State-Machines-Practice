01a80ef098fd6b42d92e8c5b79af2b82b0828bc2
Lidia <lia250930@gmail.com>
2022-02-27 23:13:04 +0100

    harpoon intento 2 puntos

--- Assets/FSMs/BoatHarpoon/FSM_Harpoon.cs
+++ Assets/FSMs/BoatHarpoon/FSM_Harpoon.cs
@@ -6,28 +6,36 @@ using Steerings;
 namespace FSM
 {
     [RequireComponent(typeof(Arrive))]
-    [RequireComponent(typeof(Seek))]
-    [RequireComponent(typeof(SHARK_Blackboard))]
+    [RequireComponent(typeof(HARPOON_Blackboard))]
     public class FSM_Harpoon : FiniteStateMachine
     {
+        
         public enum State
         {
             INITIAL,
-            WANDER,
+            GO_TO_A,
+            GO_TO_B,
             ATTACK_SHARK,
             PICK_HARPOON
         };
 
         public State currentState = State.INITIAL;
-        private FISH_Blackboard blackboard;
+
+        private HARPOON_Blackboard blackboard;
+        private Arrive arrive;
 
         // Start is called before the first frame update
         void Start()
         {
+            arrive = GetComponent<Arrive>();
+            blackboard = GetComponent<HARPOON_Blackboard>();
 
+            arrive.enabled = false;
         }
         public override void Exit()
         {
+            arrive.enabled = false;
+
             base.Exit();
         }
 
@@ -42,7 +50,23 @@ namespace FSM
             switch (currentState)
             {
                 case State.INITIAL:
-                    ChangeState(State.WANDER);
+                    ChangeState(State.GO_TO_A);
+                    break;
+                case State.GO_TO_A:
+                    if ((transform.position - blackboard.attractorA.transform.position).magnitude < blackboard.attractorReachedRadious)
+                    {
+                        ChangeState(State.GO_TO_B);
+                    }
+                    break;
+                case State.GO_TO_B:
+                    if ((transform.position - blackboard.attractorB.transform.position).magnitude < blackboard.attractorReachedRadious)
+                    {
+                        ChangeState(State.GO_TO_A);
+                    }
+                    break;
+                case State.ATTACK_SHARK:
+                    break;
+                case State.PICK_HARPOON:
                     break;
             }
         }
@@ -52,11 +76,25 @@ namespace FSM
             // ENTER STATE LOGIC. Depends on newState
             switch (newState)
             {
+                case State.GO_TO_A:
+                    arrive.target = blackboard.attractorA; //target gusano seleccionado
+                    arrive.enabled = true;
+                    break;
+                case State.GO_TO_B:
+                    arrive.target = blackboard.attractorB; //target gusano seleccionado
+                    arrive.enabled = true;
+                    break;
             }
 
             // EXIT STATE LOGIC. Depends on current state
             switch (currentState)
             {
+                case State.GO_TO_A:
+                    arrive.enabled = false;
+                    break;
+                case State.GO_TO_B:
+                    arrive.enabled = false;
+                    break;
             }
 
             currentState = newState;
---
+++ Assets/FSMs/BoatHarpoon/HARPOON_Blackboard.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HARPOON_Blackboard : MonoBehaviour
+{
+    [Header("Attractors")]
+    public GameObject attractorA;
+    public GameObject attractorB;
+
+    public float attractorReachedRadious = 10.0f;
+
+}
+
